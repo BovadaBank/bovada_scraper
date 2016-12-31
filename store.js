2a007daf5fb7052231624e1362ea22d73bfacdc0
Jon Kolman <jonathankolman@gmail.com>
2016-12-31 05:25:09 -0500

    unneeded

--- store.js
+++ store.js
@@ -1,7 +1,5 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import { urls } from './reducers'
 
 export const store = createStore(combineReducers({
-  urls
 }), applyMiddleware(thunk))
